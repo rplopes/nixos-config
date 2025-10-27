bc65ae31a057382c740f62a40aae204b2b447c01
Ricardo Lopes <mail@ricardolopes.net>
2025-10-27 23:29:49 +0000

    Add GC to delete old nix data

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,11 @@
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
 
   # Bootloader.
   boot = {

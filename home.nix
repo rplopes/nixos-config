68bd6cba513a5e43e698935684fd4bc850ef6622
Ricardo Lopes <mail@ricardolopes.net>
2025-10-28 01:44:13 +0000

    Set alacritty theme

--- home.nix
+++ home.nix
@@ -44,6 +44,22 @@
     enable = true;
     settings = {
       env.TERM = "xterm-256color";
+      colors = {
+        primary = {
+          background = "#1d2021";
+          foreground = "#ebdbb2";
+        };
+        normal = {
+          black   = "#282828";
+          red     = "#cc241d";
+          green   = "#98971a";
+          yellow  = "#d79921";
+          blue    = "#458588";
+          magenta = "#b16286";
+          cyan    = "#689d6a";
+          white   = "#a89984";
+        };
+      };
       font = {
         size = 10;
       };

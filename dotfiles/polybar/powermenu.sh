d2c6e857c215f5606db9992ed2d9120e5e53ba7e
Ricardo Lopes <mail@ricardolopes.net>
2026-03-29 10:31:52 +0100

    Make power menu horizontal like volume

--- dotfiles/polybar/powermenu.sh
+++ dotfiles/polybar/powermenu.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-chosen=$(printf "Shutdown\nReboot\nSuspend\nLock\nLog out" | rofi -dmenu -i -p " ")
+ICON_SHUTDOWN=$'\uf011'
+ICON_REBOOT=$'\uf01e'
+ICON_SUSPEND=$'\uf186'
+ICON_LOCK=$'\uf023'
+ICON_LOGOUT=$'\uf2f5'
+
+chosen=$(printf "%s\n%s\n%s\n%s\n%s" "$ICON_LOCK" "$ICON_SUSPEND" "$ICON_REBOOT" "$ICON_LOGOUT" "$ICON_SHUTDOWN" \
+  | rofi -dmenu -p "Power" -theme ~/.config/rofi/powermenu.rasi)
 
 case "$chosen" in
-  Shutdown) systemctl poweroff ;;
-  Reboot)   systemctl reboot ;;
-  Suspend)  systemctl suspend ;;
-  Lock)     i3lock ;;
-  "Log out") i3-msg exit ;;
+  "$ICON_SHUTDOWN") systemctl poweroff ;;
+  "$ICON_REBOOT")   systemctl reboot ;;
+  "$ICON_SUSPEND")  systemctl suspend ;;
+  "$ICON_LOCK")     i3lock ;;
+  "$ICON_LOGOUT")   i3-msg exit ;;
 esac
